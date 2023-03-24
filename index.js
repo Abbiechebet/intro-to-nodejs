ca7a2d5aaef92bb925ca90779e89915c1dca84db
Abbiechebet <abigaelchebet85@gmail.com>
2023-03-24 17:59:21 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+log in to the console:
+ your name, 
+ your preferred programming language, 
+where you see yourself in the next 1 year.
+ */
+const object = {
+    myName: 'Abigael Chebet',
+    progLanguage: 'JavaScript',
+    nextOneYear: 'I see myself in a Tech environment contributing my entire skills for the growth of a company and simultaneously gaining the knowledge from the company'
+}
+const{myName, progLanguage, nextOneYear } = object;
+//console.log(object);
+console.log('My name is ' + myName +'.'+ ' My preferred programming language is '+ progLanguage+'.'+ ' In the next 1 year ' + nextOneYear +'.');
